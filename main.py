e440cd120f7f66b3e430605cc13efb920ca0c9cd
sarah castorena <ceanz@yahoo.com>
2022-10-11 21:52:00 -0700

    new commit

--- main.py
+++ main.py
@@ -2,11 +2,15 @@
 # ******************************
 # Make your Code
 # ******************************
-strval = input().split()
-numbers = []
-for v in strval:
-	numbers.append(int(v))
-print (numbers)
+
+strval = input('Enter numbers: ')
+numbers = list(map(int,strval.split()))
+
+target = int(input('Enter a target number: '))
+for i in range(len(numbers)):
+	amount = numbers.count(target)
+
+print(amount)	
 # the below llin 11 are same as the lines from 5 to 8
 # numbers = list(map(int, strval))
 # print (numbers)
